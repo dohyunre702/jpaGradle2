0b1c947702a6b698e22a56159c4c0b4e8c868d80
dohyu <dohyunre702@gmail.com>
2022-12-17 21:28:20 +0900

    ch9. 영속성 전이 적용

--- src/test/java/com/springboot/jpa/data/repository/ProviderRepositoryTest.java
+++ src/test/java/com/springboot/jpa/data/repository/ProviderRepositoryTest.java
@@ -4,6 +4,7 @@ import com.springboot.jpa.data.dto.repository.ProductRepository;
 import com.springboot.jpa.data.dto.repository.ProviderRepository;
 import com.springboot.jpa.entity.Product;
 import com.springboot.jpa.entity.Provider;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -54,5 +55,40 @@ public class ProviderRepositoryTest {
         for(Product product : products) {
             System.out.println(product);
         }
-    }    
+    }
+
+    @Test //영속성 전이 테스트
+    void cascadeTest() {
+        Provider provider = savedProvider("새로운 공급업체");
+
+        Product product1 = savedProduct("상품1", 500, 1000);
+        Product product2 = savedProduct("상품2", 100, 2000);
+        Product product3 = savedProduct("상품3", 750, 5000);
+
+        //연관관계 매핑
+        product1.setProvider(provider);
+        product2.setProvider(provider);
+        product3.setProvider(provider);
+
+        provider.getProductList().addAll(Lists.newArrayList(product2, product2, product3));
+
+        providerRepository.save(provider);
+
+    }
+
+    private Provider savedProvider(String name) {
+        Provider provider = new Provider();
+        provider.setName(name);
+
+        return provider;
+    }
+
+    private Product savedProduct(String name, Integer price, Integer stock) {
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        product.setStock(stock);
+
+        return product;
+    }
 }
