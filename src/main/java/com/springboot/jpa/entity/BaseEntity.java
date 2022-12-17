004dad7099b01338954eb63d4c573e271ac84a0c
dohyu <dohyunre702@gmail.com>
2022-12-17 16:45:02 +0900

    ch8. Auditing기능 적용

---
+++ src/main/java/com/springboot/jpa/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.springboot.jpa.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
